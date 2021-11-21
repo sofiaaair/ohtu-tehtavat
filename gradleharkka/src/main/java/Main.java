942ded89a6e979919e2f73d0b5f70761b3395ed9
sofiaaair <sofia.airas@hotmail.com>
2021-11-21 18:41:11 +0200

    tehtavat 1-3

---
+++ gradleharkka/src/main/java/Main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import ohtu.Multiplier;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("kuka olet: ");
+        String nimi = scanner.nextLine();
+
+        System.out.println("Hello " + nimi);
+    }
+}
